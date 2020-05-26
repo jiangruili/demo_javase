3df82d0dd3391dcdbb028d9e5f2ccc5fa907cf9c
jiangruili <18080817325@qq.com>
2020-05-26 14:24:32 +0800

    初始化JavaSE项目

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+/**
+ * <p> Hello World程序，测试一下环境是否正常
+ * @version 1
+ * @author 蒋睿立
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World！");
+    }
+
+}
